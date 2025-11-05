ba112e8ac86dbe81d762221bc32c86f6b276b839
saurabh12571257 <davesaurabh59@gmail.com>
2025-11-05 13:33:07 +0530

    request validate

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -12,55 +12,41 @@ import (
 	"time"
 
 	"github.com/saurabh/students-api/internal/config"
+	student "github.com/saurabh/students-api/internal/http/handlers/students"
 )
 
 func main() {
-	//load config
-
 	cfg := config.MustLoad()
-	// database setup
 
-	// setup routers
 	router := http.NewServeMux()
+	router.HandleFunc("/api/students", student.New())
 
-	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-
-		w.Write([]byte("Welcome to Students API"))
-	})
-
-	// http server setup
-	server := http.Server{
+	server := &http.Server{
 		Addr:    cfg.Addr,
 		Handler: router,
 	}
 
-	slog.Info("Starting server...", slog.String("addr", cfg.Addr))
+	slog.Info("Starting server", "addr", cfg.Addr)
 	fmt.Println("Starting server on port", cfg.Addr)
 
 	done := make(chan os.Signal, 1)
-
 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
 	go func() {
-		err := server.ListenAndServe()
-		if err != nil {
-			log.Fatal("Failed to start server")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Failed to start server: %v", err)
 		}
-
 	}()
 
 	<-done
-
 	slog.Info("Stopping server...")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	err := server.Shutdown(ctx)
-	if err != nil {
-		slog.Error("Failed to stop server", slog.String("error", err.Error()))
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Failed to stop server", "error", err)
 	}
 
 	slog.Info("Server stopped gracefully")
-
 }
---
+++ internal/utils/response/response.go
@@ -0,0 +1,51 @@
+package response
+
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type Response struct {
+	Status string `json:"status"`
+	Error  string `json:"error"`
+}
+
+const (
+	statusOk    = "ok"
+	statusError = "error"
+)
+
+func WriteJson(w http.ResponseWriter, status int, data any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	return json.NewEncoder(w).Encode(data)
+
+}
+
+func GeneralError(err error) Response {
+	return Response{
+		Status: statusError,
+		Error:  err.Error(),
+	}
+}
+
+func ValidationError(errs validator.ValidationErrors) Response {
+	var errMsgs []string
+
+	for _, err := range errs {
+		switch err.ActualTag() {
+		case "required":
+			errMsgs = append(errMsgs, err.Field()+" is required")
+		default:
+			errMsgs = append(errMsgs, err.Field()+" is invalid")
+		}
+	}
+	return Response{
+		Status: statusError,
+		Error:  strings.Join(errMsgs, ", "),
+	}
+}
